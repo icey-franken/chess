7ffdf33b4b3d17810f99a7b78a3f3e608599da2b
Isaac <finken.isaac@gmail.com>
2020-09-11 18:55:51 -0500

    Part way done with BST project.

--- W17D5/python-binary-tree/tree_node.py
+++ W17D5/python-binary-tree/tree_node.py
@@ -1,11 +1,38 @@
 # TODO: Implement TreeNode class
 
-node_a = TreeNode('a')
+class TreeNode():
+    def __init__(self, value):
+        self._value = value
+        self._left = None
+        self._right = None
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, node):
+        self._left = node
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, node):
+        self._right = node
+
+
+# node_a = TreeNode('a')
 # print(node_a.value)         # 'a'
 # print(node_a.right)         # None
 # print(node_a.left)          # None
 
-node_b = TreeNode('b')
-node_a.left = node_b
+# node_b = TreeNode('b')
+# node_a.left = node_b
 # print(node_a.left.value)    # 'b'
 # print(node_a.right)         # None
--- W17D5/python-bst/bst_alternate_starter.py
+++ W17D5/python-bst/bst_alternate_starter.py
@@ -1,48 +1,94 @@
 class Node():
-  '''
-  '''
-  def __init__(self, value):
-    self._value = value
-    self._left = None
-    self._right = None
+    '''
+    '''
+
+    def __init__(self, value):
+        self._value = value
+        self._left = None
+        self._right = None
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, node):
+        self._left = node
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, node):
+        self._right = node
+
 
 class BST():
-  '''
-  '''
-  def __init__(self):
-    self._root = None
+    '''
+    '''
 
-  def insert_value(self, value, current_node=False):
-    ''' '''
-    # TODO: Implement node value insertion method
-    print(f"insert {value} here!")
+    def __init__(self):
+        self._root = None
 
-  # TODO: Implement iterative search method
-  def search_iteratively(self, value):
-    pass
+    @property
+    def root(self):
+        return self._root
 
-  # TODO: Implement recursive search method
-  def search_recursively(self, value, current_node=False):
-    pass
+    def insert_value(self, value, current_node=False):
+        ''' '''
+        # TODO: Implement node value insertion method
+        insertedNode = Node(value)
+
+        if current_node is False and self._root is None:
+            self._root = insertedNode
+            return value
+        elif current_node is False:
+            current_node = self._root
+
+        if value < current_node.value:
+            if current_node.left is not None:
+                return self.insert_value(value, current_node.left)
+            else:
+                current_node.left = insertedNode
+        elif value >= current_node.value:
+            if current_node.right is not None:
+                return self.insert_value(value, current_node.right)
+            else:
+                current_node.right = insertedNode
+        return value
+
+    # TODO: Implement iterative search method
+
+    def search_iteratively(self, value):
+        pass
+
+    # TODO: Implement recursive search method
+    def search_recursively(self, value, current_node=False):
+        pass
 
 
 tree = BST()
 print(tree._root)                         # None
 
 # 1. Test node value insertion
-tree.insert_value(3)
+# tree.insert_value(3)
 tree.insert_value(10)
 tree.insert_value(5)
 tree.insert_value(16)
 tree.insert_value(1)
 tree.insert_value(7)
 tree.insert_value(16)
-# print(tree._root._value)                  # 10
-# print(tree._root._left._value)            # 5
-# print(tree._root._right._value)           # 16
-# print(tree._root._left._left._value)      # 1
-# print(tree._root._left._right._value)     # 7
-# print(tree._root._right._right._value)    # 16
+print(tree.root.value)                  # 10
+print(tree.root.left.value)            # 5
+print(tree.root.right.value)           # 16
+print(tree.root.left.left.value)      # 1
+print(tree.root.left.right.value)     # 7
+print(tree.root.right.right.value)    # 16
 
 # # 2. Test iterative search
 # empty_tree = BinarySearchTree(2)
