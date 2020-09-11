c2c04edbffe095a12fb8b586ce764a257abbdedc
Isaac <finken.isaac@gmail.com>
2020-09-11 12:40:24 -0500

    Finished tic tac toe practice.

--- W17D5/python-tic-tac-toe/tic_tac_toe.py
+++ W17D5/python-tic-tac-toe/tic_tac_toe.py
@@ -1,4 +1,5 @@
 import random
+shuffle = random.shuffle
 
 
 # This function must be completed
@@ -12,21 +13,24 @@ def space_value(board, index):
     Arguments: board: An array of nine strings index: The value in the board to
     interrogate
     """
-    pass
+    spaceValue = board[index]
+    if spaceValue == ' ':
+        return str(index)
+    return spaceValue
 
 
 def draw_board(board):
     """This function prints out the board that it was passed."""
     print("   |   |")
-    print(f" {space_value(board, 7)} | {space_value(board, 8)} | {space_value(board, 9)}")
+    print(f''' {space_value(board, 7)} | {space_value(board, 8)} | {space_value(board, 9)}''')
     print("   |   |")
     print("-----------")
     print("   |   |")
-    print(f" {space_value(board, 4)} | {space_value(board, 5)} | {space_value(board, 6)}")
+    print(f''' {space_value(board, 4)} | {space_value(board, 5)} | {space_value(board, 6)}''')
     print("   |   |")
     print("-----------")
     print("   |   |")
-    print(f" {space_value(board, 1)} | {space_value(board, 2)} | {space_value(board, 3)}")
+    print(f''' {space_value(board, 1)} | {space_value(board, 2)} | {space_value(board, 3)}''')
     print("   |   |")
 
 
@@ -34,8 +38,8 @@ def get_player_and_computer_letters():
     """
     Lets the player type which letter they want to be.
 
-    Returns a list with the player's letter as the first item and the computer's
-    letter as the second item.
+    Returns a list with the player's letter as the first item and
+    the computer's letter as the second item.
     """
     letter = ""
     while not (letter == "X" or letter == "O"):
@@ -52,17 +56,20 @@ def play_again():
     """
     Determines if the player wants to play again.
 
-    It should as the player if they want to play again, then read input from the
-    player to determine if they typed some value that begins with the letter
-    "y". If the value does begin with "y", then return True. Otherwise, return
-    False.
+    It should as the player if they want to play again, then read input from
+    the player to determine if they typed some value that begins with the
+    letter "y". If the value does begin with "y", then return True.
+    Otherwise, return False.
     """
-    pass
+    answer = input('Do you want to play again? ')
+    return answer[0].lower() == 'y'
 
 
 def make_move(board, letter, move):
     """Set the board at index move to the provided letter"""
+    print('line 68', type(move))
     board[move] = letter
+    return board
 
 
 # This function must be completed
@@ -73,7 +80,21 @@ def is_winner(board, letter):
     Given the board and the player's letter, this function returns True if that
     player has won.
     """
-    pass
+    winningCombos = [
+        (1, 2, 3,),
+        (4, 5, 6,),
+        (7, 8, 9,),
+        (1, 4, 7,),
+        (2, 5, 8,),
+        (3, 6, 9,),
+        (1, 5, 9,),
+        (3, 5, 7),
+    ]
+    for winningCombo in winningCombos:
+        if len(list(filter(
+                lambda spot: letter == board[spot], winningCombo))) == 3:
+            return True
+    return False
 
 
 def is_space_free(board, move):
@@ -86,12 +107,25 @@ def get_player_move(board):
     """
     Prompt the player for their move and return their value as an integer.
 
-    This function insures that the board space in which the player wants to play
-    is empty. If the player indicates a square that already has a value in it,
-    then the function tells the player that is an invalid move and prompts the
-    player, again, for a value.
+    This function ensures that the board space in which the player wants to
+    play is empty. If the player indicates a square that already has a value
+    in it, then the function tells the player that is an invalid
+    move and prompts the player, again, for a value.
     """
-    pass
+    validMove = False
+    while not validMove:
+        playerMove = input('Select a number for your move: ')
+        try:
+            playerMove = int(float(playerMove)//1)
+        except (TypeError, ValueError):
+            print('Invalid move - try again.')
+            continue
+        if 0 < playerMove < 10 and is_space_free(board, playerMove):
+            # print('valid move', playerMove, type(playerMove))
+            validMove = True
+        else:
+            print('Invalid move - try again.')
+    return playerMove
 
 
 # This function must be completed
@@ -102,7 +136,10 @@ def get_random_move(board):
 
     To get nice random moves, consider using the random.shuffle method, here.
     """
-    pass
+    openSpaces = [(board[i], i) for i in range(1, 10) if board[i] == ' ']
+    shuffle(openSpaces)
+    if len(openSpaces) != 0:
+        return openSpaces[0][1]
 
 
 # This function must be completed
@@ -111,9 +148,14 @@ def is_board_full(board):
     Return True if every space on the board has been taken. Otherwise return
     False.
     """
-    pass
+    for i in range(1, 10):
+        if is_space_free(board, i) is True:
+            return False
+    return True
+
 
 print("Welcome to Tic Tac Toe!")
+
 
 while True:
     board = [" "] * 10
@@ -128,8 +170,7 @@ while True:
         if turn == "player":
             draw_board(board)
             move = get_player_move(board)
-            make_move(board, player, move)
-
+            board = make_move(board, player, move)
             if is_winner(board, player):
                 draw_board(board)
                 print("Hooray! You have won the game!")
